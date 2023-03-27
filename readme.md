451804db4cf100b16a5ba44a32cd3289197dcb24
Aslan Abaev <hedgehog.asl@yandex.ru>
2023-03-27 20:44:22 +0300

    first commit

---
+++ readme.md
@@ -0,0 +1,10 @@
+# Стартовый шаблон
+
+Данный шаблон включает:
+1. React + TS
+2. eslint + prettier
+3. react-router
+4. emotion
+
+dev версия запускается командой npm run dev
+Для тестов используется команда npm run test
---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+
+export function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
+
+export function WrappedApp() {
+  return (
+    <Router>
+      <div>
+        <App />
+      </div>
+    </Router>
+  );
+}
