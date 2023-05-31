c0ff0518f4884bb7de75a093f3f06d36ae21f34c
Aslan Abaev <hedgehog.asl@yandex.ru>
2023-05-31 18:18:50 +0300

    dev useEditPost

--- src/pages/Posts/index.tsx
+++ src/pages/Posts/index.tsx
@@ -1,11 +1,20 @@
 import { PostWrapper } from '@/pages/Posts/index.styles';
 import { IPostProps, Post } from '@/pages/Posts/partials/Post';
-import useEditPosts from "@/pages/Posts/useEditPosts";
+import useEditPosts from '@/pages/Posts/useEditPosts';
 
 export default function Posts() {
-  const { posts } = useEditPosts();
+  const { posts, addNewPost } = useEditPosts();
   const renderedPosts = posts.map((item: IPostProps) => (
-    <Post key={item.id} title={item.title} description={item.description} />
+    <Post
+      key={item.id + item.title + item.description}
+      title={item.title}
+      description={item.description}
+    />
   ));
-  return <PostWrapper>{renderedPosts}</PostWrapper>;
+  return (
+    <PostWrapper>
+      <button onClick={() => addNewPost()}>Add new post</button>
+      {renderedPosts}
+    </PostWrapper>
+  );
 }
--- src/pages/Posts/useEditPosts.ts
+++ src/pages/Posts/useEditPosts.ts
@@ -1,23 +1,44 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { faker } from '@faker-js/faker';
 import { IPostProps } from '@/pages/Posts/partials/Post';
 
 export default function useEditPosts() {
   const [posts, setPosts] = useState<IPostProps[]>([]);
+  const apiUrl = 'https://646b784e7d3c1cae4ce3d933.mockapi.io/api/Products';
+
+  async function fetchData() {
+    try {
+      const response = await axios(apiUrl);
+      setPosts(response.data);
+    } catch (error: unknown) {
+      console.log(error.message);
+    }
+  }
 
   useEffect(() => {
-    const apiUrl = 'https://646b784e7d3c1cae4ce3d933.mockapi.io/api/Products';
-    axios.get(apiUrl).then((response) => {
-      console.log(response.data);
-      setPosts(response.data);
-    });
+    fetchData();
   }, []);
 
-  function addNewPost() {
+  async function addNewPost() {
+    try {
+      const newPostData = {
+        title: `${faker.commerce.product()}`,
+        description: `${faker.commerce.productDescription()}`,
+        id: `${posts.length + 1}`,
+      };
+      const response = await axios.post(apiUrl, newPostData);
 
+      setPosts((prev) => [...prev, newPostData]);
+    } catch (error: unknown) {
+      console.log(error.message);
+    }
+
+    console.log(posts);
   }
   return {
     posts,
     setPosts,
+    addNewPost,
   };
 }
