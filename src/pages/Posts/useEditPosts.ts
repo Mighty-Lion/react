5872c6c0dfa48574a9914a0de0d238c2441a5691
Aslan Abaev <hedgehog.asl@yandex.ru>
2023-06-01 21:22:51 +0300

    dev useEditPosts

--- src/pages/Posts/useEditPosts.ts
+++ src/pages/Posts/useEditPosts.ts
@@ -2,17 +2,20 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { faker } from '@faker-js/faker';
 import { IPostProps } from '@/pages/Posts/partials/Post';
+import { useToastNotifications } from '@/components/ToastMessage/useToastNotifications';
 
 export default function useEditPosts() {
   const [posts, setPosts] = useState<IPostProps[]>([]);
   const apiUrl = 'https://646b784e7d3c1cae4ce3d933.mockapi.io/api/Products';
-
+  const toastNotifications = useToastNotifications();
   async function fetchData() {
     try {
       const response = await axios(apiUrl);
       setPosts(response.data);
-    } catch (error: unknown) {
-      console.log(error.message);
+      toastNotifications.handleFailure('Error!');
+    } catch (error) {
+      console.log('handelSendReminder', error);
+      toastNotifications.handleFailure('Error!');
     }
   }
 
